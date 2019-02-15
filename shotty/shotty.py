cc16362dd3a19e54d9a42497da884902018b9e03
air <air@air.local>
2019-02-15 10:10:39 +0000

    Wait for instances to stop and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,6 +14,15 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """shotty manages snapshots"""
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
@@ -50,9 +59,71 @@ def start_instances(project):
     "Start EC2 instances"
     instances = filter_instances(project)
     for i in instances:
-        print("Starting {0}...".format(i.id))
+        print("Starting instances of {0}...".format(i.id))
         i.start()
     return
 
+@volumes.command('list')
+@click.option('--project', default=None, help='only volumes for project (tag Project:<name>)')
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+         print(i)
+         for v in i.volumes.all():
+             print(", ".join((
+                 v.id,
+                 i.id,
+                 v.state,
+                 str(v.size) + "Gib",
+                 v.encrypted and "Encrypted" or "Not Encrypted"
+             )))
+    return
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+@snapshots.command('list')
+@click.option('--project', default=None, help='only snapshots for project (tag Project:<name>)')
+def list_volumes(project):
+    "List EC2 volumes snapshots"
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(",".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+@instances.command('snapshot', help="create snapshots")
+@click.option('--project', default=None, help='only snapshots for project (tag Project:<name>)')
+def create_snapshot(project):
+    "Create volumes snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("Creating snaposhots of {0}...".format(v.id))
+            v.create_snapshot(Description="by Leilei")
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+    print("Job's done!")    
+    return
+
+
 if __name__== '__main__':
-    instances()
+    cli()
